eb4c9d777036bc577edec9aec10be57dcb89bba1
IniZio <digit4free@gmail.com>
2016-04-04 14:15:30 +0800

    tested with shopsy default paths for css files

--- application/views/site/template/css_files.php
+++ application/views/site/template/css_files.php
@@ -1,24 +1,24 @@
-<link href="css/front/bootstrap.css" rel="stylesheet">
-<link href="css/front/font-awesome.css" rel="stylesheet">
-<link href="css/front/main.css" rel="stylesheet">
-<link href="css/front/deal.css" rel="stylesheet">
-<link href="css/front/browse.css" rel="stylesheet">
-<link href="css/front/home.css" rel="stylesheet">
-<link href="css/front/art.css" rel="stylesheet">
-<link href="css/front/seller.css" rel="stylesheet">
-<link href="css/front/custom.css" rel="stylesheet"> 
-<link href="css/site/responsive-dev.css" rel="stylesheet"> 
-<link rel="stylesheet" type="text/css" media="all" href="css/site/shopsy_style.css"/>
-<link rel="stylesheet" type="text/css" media="all" href="css/site/shopsy_style_1.css"/>
-<link rel="stylesheet" type="text/css" media="all" href="css/site/account_master.css"/>
-<link rel="stylesheet" type="text/css" media="all" href="css/site/popup.css"/>
-<link rel="stylesheet" type="text/css" media="all" href="css/site/help.css"/>
-<link rel="stylesheet" type="text/css" media="all" href="css/front/auction.css"/>
-<!---<link rel="stylesheet" type="text/css" media="all" href="css/site/my_shop.css"/>
- <link href="css/front/new-style.css" rel="stylesheet">  -->
-<link href="css/front/edit-css.css" rel="stylesheet">
-<link href="css/site/shop-add.css" rel="stylesheet">
-<link href="css/front/menu-horizontal.css" rel="stylesheet">
-<link href="css/front/zo-cas-style.css" rel="stylesheet">
-<link href="css/front/style-responsive.css" rel="stylesheet">
-<link href="css/front/responsive-style-sheet.css" rel="stylesheet">
+<link href="css/default/front/bootstrap.css" rel="stylesheet">
+<link href="css/default/front/font-awesome.css" rel="stylesheet">
+<link href="css/default/front/main.css" rel="stylesheet">
+<link href="css/default/front/deal.css" rel="stylesheet">
+<link href="css/default/front/browse.css" rel="stylesheet">
+<link href="css/default/front/home.css" rel="stylesheet">
+<link href="css/default/front/art.css" rel="stylesheet">
+<link href="css/default/front/seller.css" rel="stylesheet">
+<link href="css/default/front/custom.css" rel="stylesheet"> 
+<link href="css/default/site/responsive-dev.css" rel="stylesheet"> 
+<link rel="stylesheet" type="text/css" media="all" href="css/default/site/shopsy_style.css"/>
+<link rel="stylesheet" type="text/css" media="all" href="css/default/site/shopsy_style_1.css"/>
+<link rel="stylesheet" type="text/css" media="all" href="css/default/site/account_master.css"/>
+<link rel="stylesheet" type="text/css" media="all" href="css/default/site/popup.css"/>
+<link rel="stylesheet" type="text/css" media="all" href="css/default/site/help.css"/>
+<link rel="stylesheet" type="text/css" media="all" href="css/default/front/auction.css"/>
+<!---<link rel="stylesheet" type="text/css" media="all" href="css/default/site/my_shop.css"/>
+ <link href="css/default/front/new-style.css" rel="stylesheet">  -->
+<link href="css/default/front/edit-css.css" rel="stylesheet">
+<link href="css/default/site/shop-add.css" rel="stylesheet">
+<link href="css/default/front/menu-horizontal.css" rel="stylesheet">
+<link href="css/default/front/zo-cas-style.css" rel="stylesheet">
+<link href="css/default/front/style-responsive.css" rel="stylesheet">
+<link href="css/default/front/responsive-style-sheet.css" rel="stylesheet">
