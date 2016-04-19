cc9e0ec0e2f1307503beea9c0c3bf0fe350e2412
iniz <digit4free@gmail.com>
2016-04-19 11:15:34 +0800

    file for stating the history of commits that have been synced with real site

--- convey/import_etsy_listings.php
+++ convey/import_etsy_listings.php
@@ -1,7 +1,7 @@
 <?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-$route['import-items'] = "site/product_import/upload_form";
-$route['etsy-import'] = "site/product_import/upload_etsy_form";
+// $route['import-items'] = "site/product_import/upload_form";
+// $route['etsy-import'] = "site/product_import/upload_etsy_form";
 
 /* End of file import_etsy_listings.php */
 /* Location: ./application/config/import_etsy_listings.php */
