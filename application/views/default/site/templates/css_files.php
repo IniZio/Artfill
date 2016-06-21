aa12ed0dbe307c53498f4a1027d11006e4b081c4
inizio <digit4free@gmail.com>
2016-06-21 22:11:26 +0800

    remove cdn bootstrap

--- application/views/default/site/templates/css_files.php
+++ application/views/default/site/templates/css_files.php
@@ -1,4 +1,4 @@
-<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+<!-- <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css"> -->
 <link href="css/default/front/bootstrap.css" rel="stylesheet">
 <link href="css/default/front/font-awesome.css" rel="stylesheet">
 <link href="css/default/front/main.css" rel="stylesheet">
