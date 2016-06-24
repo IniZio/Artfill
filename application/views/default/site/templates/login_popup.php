21de1c5ed31c25ba099227074922e2cf85dbbcdb
inizio <digit4free@gmail.com>
2016-06-24 15:01:44 +0800

    register pop up closed button

--- application/views/default/site/templates/login_popup.php
+++ application/views/default/site/templates/login_popup.php
@@ -87,7 +87,7 @@ if($this->session->userdata('rUrl') != ''){
 			<div class="tabbable-panel">
 			<div style="margin:5px;border: 2px dashed #8adbd4; border-radius:5px;">
 				<span style="float:right;">
-					<a class="btn btn-default " href="javascript:void(0);" data-dismiss="modal">
+					<a class="btn btn-default btn-circle" href="javascript:void(0);" data-dismiss="modal">
 					<?php echo af_lg('X', 'X'); ?>
 					</a>
 				</span>
