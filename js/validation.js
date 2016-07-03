0ea28964024be5fe1bbe11b45819bf92bddc7ac8
kycheungat <kinyu_cheung@yahoo.com.hk>
2016-07-03 14:38:30 +0800

    login pop up

--- js/validation.js
+++ js/validation.js
@@ -3477,7 +3477,7 @@ $(evt).css({'opacity':'0.4','cursor':'progress'});
 					}
 				
             } else {
-				$("#signin").trigger('click');
+				$("#signin").modal('show');
 				//window.location.href = baseURL+'#signin';
 				//window.location.href = baseURL+'login?action='+json.next_url+'&redirect='+redirect;
 			}
