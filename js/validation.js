ec5b4eab7d6d155870b0ceeead97da524a0925fc
inizio <digit4free@gmail.com>
2016-07-04 16:10:03 +0800

    Revert "Revert "add detect #signin and #signup""
    This reverts commit 914a6bd2149ee2929ef8d2b760ec0aadab624f87.
    minor tweak to product card image
    
    
    Revert "Revert "add detect #signin and #signup""
    
    This reverts commit 0d4d6d3036f562c6fb7d226e7b7ef2ed6a731d51.

--- js/validation.js
+++ js/validation.js
@@ -1283,6 +1283,9 @@ function loginVal(evt){
 					l.stop();
 
                 } else {
+                	$("#loginloadErr").style.color="green";
+                	$("#loginloadErr").html('登錄成功!');
+					$('#loginloadErr').show();
                 	$(evt).attr('action', 'site/user/login_user');
                 	$(evt).attr('onSubmit', '');
                 	$(evt).off('onSubmit').submit();
