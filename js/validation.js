a5017613b368a85da6079d27f3a53057da01390e
kycheungat <kinyu_cheung@yahoo.com.hk>
2016-07-04 22:41:35 +0800

    like/unlike function

--- js/validation.js
+++ js/validation.js
@@ -3468,12 +3468,14 @@ $(evt).css({'opacity':'0.4','cursor':'progress'});
 					   //alert("fav");
 					   //$('#fav').hasClass('fav-icon-sel')
 					   $('#prodfav').removeClass('fav-icon-sel');
-					   $('#prodfav').addClass('fa fa-heart');
+					   $('#prodfav').addClass('fa');
+					   $('#prodfav').addClass('fa-heart');
 					   
 					}else{
 					   //alert("Unfav");
 					   //$('#fav').hasClass('fa fa-heart')
-					   $('#prodfav').removeClass('fa fa-heart');
+					   $('#prodfav').removeClass('fa');
+					   $('#prodfav').removeClass('fa-heart');
 					   $('#prodfav').addClass('fav-icon-sel');
 					}
 				
