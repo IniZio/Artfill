f31588395b4d723007692acbae8ca23e44c2d401
kycheungat <kinyu_cheung@yahoo.com.hk>
2016-07-16 00:38:47 +0800

    sidebar

--- application/views/default/site/user/sidebar.php
+++ application/views/default/site/user/sidebar.php
@@ -90,7 +90,7 @@
         </a>
 
     </li>
-	 <li <?php if($this->uri->segment(2) == 'invite-friends' ){ echo 'class="side_active"';} ?>>
+	<!-- <li <?php if($this->uri->segment(2) == 'invite-friends' ){ echo 'class="side_active"';} ?>>
 
     	<a href="settings/invite-friends" ><i class="ic-credit"></i>
         	<?php if($this->lang->line('lg_user_invite') != '') { echo stripslashes($this->lang->line('lg_user_invite')); } else echo "Invite Friends"; ?>
@@ -98,7 +98,7 @@
 
         </a>
 
-    </li>
+    </li>-->
 		
 	<?php	}
 	?>
