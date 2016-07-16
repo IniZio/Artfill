27a6b6cb3daec22722da522f3f9f313edb8f1d6a
kycheungat <kinyu_cheung@yahoo.com.hk>
2016-07-16 21:24:24 +0800

    test

--- application/views/default/site/user/sidebar.php
+++ application/views/default/site/user/sidebar.php
@@ -2,7 +2,7 @@
 <link href='http://fonts.googleapis.com/css?family=Ubuntu:300,400,700,400italic' rel='stylesheet' type='text/css'>
 <link href='http://fonts.googleapis.com/css?family=Oswald:400,300,700' rel='stylesheet' type='text/css'>
 
-<link rel="stylesheet" href="css/default/site/style-menu.css" />
+<!--<link rel="stylesheet" href="css/default/site/style-menu.css" />-->
     
 <!-- js -->
 <script src="js/jquery-1.9.1.min.js"></script>
