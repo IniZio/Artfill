16f23b4b1a770bede682e597c1cf16ddb22bd757
inizio <digit4free@gmail.com>
2016-07-18 23:25:37 +0800

    wow htaccess files missing

---
+++ sitemap/settings/files.inc.php
@@ -0,0 +1,7 @@
+<?php
+$FILES['http://localhost/']['file_enabled'] = '1';
+$FILES['http://localhost/']['file_url'] = 'http://localhost/';
+$FILES['http://localhost/']['lastmod'] = '';
+$FILES['http://localhost/']['changefreq'] = 'never';
+$FILES['http://localhost/']['priority'] = '0.0';
+?>
