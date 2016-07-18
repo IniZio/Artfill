9e94b8085cbdaa0e6a3b0174c5c1d3718f3c981e
inizio <digit4free@gmail.com>
2016-07-18 22:45:48 +0800

    localhost settings

---
+++ config-shopsy/databaseValues.php
@@ -0,0 +1,7 @@
+<?php error_reporting(0);
+	   $hostName = 'localhost';
+	   $dbUserName = 'root';
+	   $dbPassword = 'toor';
+	   $databaseName = 'artfill';
+ define('SITE_URL', 'http://localhost');
+ ?>
