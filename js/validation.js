64c603a9d45ae579b2d48b7350a2a52bedd86f3e
kycheungat <kinyu_cheung@yahoo.com.hk>
2016-07-18 22:36:26 +0800

    God bless me

--- js/validation.js
+++ js/validation.js
@@ -3477,8 +3477,8 @@ $(evt).css({'opacity':'0.4','cursor':'progress'});
 				
             } else {
 				// $("#signin").modal();
-				document.getElementById('signin-modal').click();
-				//window.location.href = baseURL+'login?action='+json.next_url+'&redirect='+redirect;
+				//document.getElementById('signin-modal').click();
+				window.location.href = baseURL+'login?action='+json.next_url+'&redirect='+redirect;
 			}
 		},
 		complete: function(){
