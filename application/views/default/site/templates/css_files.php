f1bfcfdd05b4b0977c4dd6b84caf322d0694a9fc
inizio <digit4free@gmail.com>
2016-08-02 22:17:46 +0800

    force font-awesome to work

--- application/views/default/site/templates/css_files.php
+++ application/views/default/site/templates/css_files.php
@@ -10,7 +10,6 @@
 <link href="css/default/front/custom.css" rel="stylesheet"> 
 <link href="css/default/site/responsive-dev.css" rel="stylesheet"> 
 <link rel="stylesheet" type="text/css" media="all" href="css/default/site/shopsy_style.css"/>
-<!-- <link rel="stylesheet" type="text/css" media="all" href="css/default/site/shopsy_style_1.css"/>-->
 <link rel="stylesheet" type="text/css" media="all" href="css/default/site/account_master.css"/>
 <link rel="stylesheet" type="text/css" media="all" href="css/default/site/popup.css"/>
 <link rel="stylesheet" type="text/css" media="all" href="css/default/site/help.css"/>
