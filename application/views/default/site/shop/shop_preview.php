11ff1268ebb8c2538a60a9bfaf58fb184648e77e
kycheungat <kinyu_cheung@yahoo.com.hk>
2016-08-03 23:15:26 +0800

    update

--- application/views/default/site/shop/shop_preview.php
+++ application/views/default/site/shop/shop_preview.php
@@ -129,7 +129,7 @@ $this->load->view('site/templates/shop_header');
                             <li class="seller_info">
                                     <a href="policies/<?php echo $seourl; ?>/Seller-Information"><?php if($this->lang->line('shop_sellerinformation') != '') { echo stripslashes($this->lang->line('shop_sellerinformation')); } else echo 'Seller Information'; ?></a>
                             </li>  
-							<li style="margin-left:450px;"><input type="checkbox" name="gcardaccept" id="gcardaccept" value="yes" <?php if($selectSeller_details[0]['gift_card'] == 'Yes') {echo 'checked';} ?> onchange="valchecked(this);"> <?php echo af_lg('lg_accept_gift_card',' Accept Gift Card');?></li>
+							<!--<li style="margin-left:450px;"><input type="checkbox" name="gcardaccept" id="gcardaccept" value="yes" <?php if($selectSeller_details[0]['gift_card'] == 'Yes') {echo 'checked';} ?> onchange="valchecked(this);"> <?php echo af_lg('lg_accept_gift_card',' Accept Gift Card');?></li>-->
                         </ul>
 						<?php /* <div style="float:right; width:25%; padding: 10px;">
 							<a href="shops/<?php echo $seourl; ?>/sections/All">Add shop sections</a>
@@ -149,7 +149,7 @@ $this->load->view('site/templates/shop_header');
                     	<li>
                     	<a href="policies/<?php echo $seourl; ?>/shop-policy"><?php if($this->lang->line('shop_shoppolicy') != '') { echo stripslashes($this->lang->line('shop_shoppolicy')); } else echo 'Add shop policies'; ?></a>
                     	</li>
-                    	<li style="margin-left:450px;"><input type="checkbox" name="gcardaccept" id="gcardaccept" value="yes" <?php if($selectSeller_details[0]['gift_card'] == 'Yes') {echo 'checked';} ?> onchange="valchecked(this);"> <?php echo af_lg('lg_accept_gift_card',' Accept Gift Card');?></li>
+                    	<!--<li style="margin-left:450px;"><input type="checkbox" name="gcardaccept" id="gcardaccept" value="yes" <?php if($selectSeller_details[0]['gift_card'] == 'Yes') {echo 'checked';} ?> onchange="valchecked(this);"> <?php echo af_lg('lg_accept_gift_card',' Accept Gift Card');?></li>-->
 							</ul>
                     </div>
                     <?php } ?>							
